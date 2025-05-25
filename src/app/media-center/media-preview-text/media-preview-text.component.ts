f8e532ed64dab6f5b715fdc98edeb76944e1fe11
Marcel Arndt <marcel.arndt@hotmail.com>
2025-05-25 12:12:42 +0200

    some response css work

--- src/app/media-center/media-preview-text/media-preview-text.component.ts
+++ src/app/media-center/media-preview-text/media-preview-text.component.ts
@@ -1,8 +1,9 @@
 import { Component } from '@angular/core';
+import { IconComponent } from '../../../share/icon/icon.component';
 
 @Component({
   selector: 'app-media-preview-text',
-  imports: [],
+  imports: [IconComponent ],
   templateUrl: './media-preview-text.component.html',
   styleUrl: './media-preview-text.component.scss'
 })
